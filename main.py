705d933fcdf1e4105a822753f4061e6901799bdc
Namilsky <alive6863@gmail.com>
2025-03-03 19:35:42 +0300

    third q

--- main.py
+++ main.py
@@ -18,5 +18,16 @@ def plan(prof):
     return render_template('content.html', **param)
 
 
+@app.route('/list_prof/<how_to_list>')
+def list_prof(how_to_list):
+    prof_list = ['инженер', 'строитель', 'врач']
+    param = {}
+    param['prof_list'] = prof_list
+    param['how_to_list'] = how_to_list
+    return render_template('second.html', **param)
+
+
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
