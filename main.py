ab0d3ee945824be96fa37d3540521a499010636f
Namilsky <alive6863@gmail.com>
2025-03-06 19:38:05 +0300

    q q

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import *
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -27,6 +31,39 @@ def list_prof(how_to_list):
     return render_template('second.html', **param)
 
 
+class LoginForm(FlaskForm):
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    education = StringField('Образование', validators=[DataRequired()])
+    profession = StringField('Профессия', validators=[DataRequired()])
+    sex = StringField('Пол', validators=[DataRequired()])
+    motivation = StringField('Мотивация', validators=[DataRequired()])
+    ready = BooleanField('Готовы остаться на Марсе?')
+    submit = SubmitField('Записаться')
+
+
+@app.route('/answer', methods=['GET', 'POST'])
+@app.route('/auto_answer', methods=['GET', 'POST'])
+def answer():
+    form = LoginForm()
+    if form.validate_on_submit():
+        res = {
+            "Фамилия": form.data["surname"],
+            "Имя": form.data["name"],
+            "Образование": form.data["education"],
+            "Профессия": form.data["profession"],
+            "Пол": form.data["sex"],
+            "Мотивация": form.data["motivation"],
+            "Готовы остаться на Марсе?": form.data["ready"],
+        }
+        # return "<br>".join([f"{i}: {j}" for i, j in res.items()])
+        return render_template('otvet.html', data=res)
+    return render_template('auto_answer.html', title='Авторизация', form=form)
+
+
+@app.route('/success/<data>')
+def otvet(data):
+    return ""
 
 
 if __name__ == '__main__':
